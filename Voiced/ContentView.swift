6d6ac86ac251781502bdb40e241c693404b0f2c5
Abraham Morales Arroyo <abrahamof606@gmail.com>
2024-02-27 17:29:29 -0600

    Had to change the name

--- Voiced/ContentView.swift
+++ Voiced/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Him")
+            Text("Voiced 1.0")
         }
         .padding()
     }
