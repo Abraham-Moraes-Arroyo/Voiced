343aec4bd25837ad1a01154d0879845f0ff71862
Abraham Morales Arroyo <abrahamof606@gmail.com>
2024-02-29 08:00:42 -0600

    Added the code that allows the user to drop pins onto the map. I have also added the necessary files required to make the code work.

---
+++ Voiced/locations.swift
@@ -0,0 +1,17 @@
+//
+//  locations.swift
+//  UserLocationOnMap
+//
+//  Created by Abraham Morales Arroyo on 2/29/24.
+//
+
+import Foundation
+
+
+struct locations: Codable, Equatable, Identifiable{
+    let id: UUID
+    var name: String
+    var description: String
+    var latitude: Double
+    var longitude: Double
+}
--- Voiced/number1.swift
+++ Voiced/number1.swift
@@ -9,21 +9,44 @@ import SwiftUI
 import MapKit
 
 struct number1: View {
-    // gets the user's location and displays it on the map
+    
     @State private var position: MapCameraPosition = .userLocation(fallback: .automatic)
     
+//    empty array that will store the location of the map markers that are going to be dropped.
+    @State private var locationsS = [locations]()
+    
     var body: some View {
-        Map(position: $position) {
+        MapReader{ proxy in
+            Map(position: $position) {
+//                below is where we are going to be fetching through the array and be plotting the new points that we have.
+                ForEach(locationsS) { location in
+                       Marker( location.name, coordinate: CLLocationCoordinate2D(
+                        latitude: location.latitude,
+                        longitude: location.longitude)
+                       )
+                }
+            }
+//            this is to get the coordinates of the user, based on where they tap on. Based on the following link:
+//            https://www.youtube.com/watch?v=9T-xoR7XmUc
+            //                    here is where we are going to be storing the dropped markers in to the empty array called locationsS, that has been defined in line 16
+
+            .onTapGesture { location in
+                if let coordinate = proxy.convert(location, from: .local ){
+                    let newLocation = locations(id: UUID(), name: "New Location", description: "", latitude: coordinate.latitude, longitude: coordinate.longitude)
+                    locationsS.append(newLocation)
+                }
+                
+            }
+            .mapControls {
+                MapUserLocationButton()
+                MapPitchToggle()
+            }
             
-            
+              .onAppear(){
+                  CLLocationManager().requestWhenInUseAuthorization()
+              }
         }
-        .mapControls {
-            MapUserLocationButton()
-            MapPitchToggle()
-        }
-        .onAppear(){
-            CLLocationManager().requestWhenInUseAuthorization()
-        }
+      
     }
 }
 
