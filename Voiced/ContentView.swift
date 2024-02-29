8442520f9aece66b855ed53c6f88ce08bd39944a
Abraham Morales Arroyo <abrahamof606@gmail.com>
2024-02-29 17:28:05 -0600

    Added the datamodel for the firebase calls and viewmodel.

--- Voiced/ContentView.swift
+++ Voiced/ContentView.swift
@@ -4,11 +4,11 @@
 //
 //  Created by Abraham Morales Arroyo on 2/27/24.
 //
-
+import Firebase
 import SwiftUI
 import MapKit
 import CoreLocation
-
+// this is the view, next wee need to make the view model, manages all the changes that happen to the data, calling firebase, changing numbers and all data arch. 
 struct ContentView: View {
     var body: some View {
         TabView{
