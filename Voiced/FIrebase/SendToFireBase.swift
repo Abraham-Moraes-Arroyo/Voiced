7ac5d90977e1f899caea2d8f253cc7db5bd1c702
Abraham Morales Arroyo <abrahamof606@gmail.com>
2024-03-03 12:18:25 -0600

    Attempt in pushing into collecting the information of the pindrop and having it be stored into the firebaase.

---
+++ Voiced/FIrebase/SendToFireBase.swift
@@ -0,0 +1,133 @@
+//
+//  SendToFireBase.swift
+//  Voiced
+//
+//  Created by Abraham Morales Arroyo on 3/1/24.
+//
+
+
+import SwiftUI
+import MapKit
+import Firebase
+import FirebaseFirestore
+
+struct Issuelocations: Decodable {
+    var issuelocations: [location2]
+    
+    enum CodingKeys: Stirng, CodingKey {
+        case issuelocations = "Issue Locations"
+    }
+}
+
+
+struct SendToFireBase: view{
+    var body: some View{
+        VStack{
+            Button("Push Issue")
+            // call the funciton when the butotn is tapped
+            uploadIssueLocationToFirebase()
+        }
+        
+    }
+}
+
+func uploadIssueLocationToFirebase(locations: [location2]){
+    let db = Firestore.firestore()
+    let firebaseLocations = db.collection("Locations")
+//            self.init(name: Name, category: Category, latitude: latitude, longitude: longitude)
+
+    for location in locations {
+        reverseGeocoder(coordinate: location.coordinate) { address in
+            var data: [String: Any] = [
+                "Name": location.name,
+                "Category": location.category,
+                "Latitude": location.coordinate.latitude,
+                "Longitude": location.coordinate.longitude
+                
+            ]
+            if let address = address {
+                data["Address"] = address
+            }
+            let firebaseLocation = firebase.addDocument(data: data)
+        }
+    }
+}
+//clean lines 55 and below
+func uploadIssuesLocationsToFireBase(){
+    if let path = Bundle.main.path(forResource: "Issues_Locations", ofType: "json") {
+        // reads the json data from the file
+        
+        let jsonData = try Data(contentsOf: URL(fileURLWithPath: path))
+        
+        // decoddes json file to locaitons instnace
+        
+        let decoder = JSONDecoder()
+        let issuelocations = try decoder.decode(Issuelocations)
+        
+        // calls uploadIssueLocationToFirebase()
+        uploadIssuesLocationsToFireBase(locations: Issuelocations.issueLocations)
+    } catch {
+        print("Error reading of decoding JSON \(error)")
+    } else {
+        print("JSON FILE NOT FOUND ")
+    }
+}
+
+func reverseGeocodeLocation(coordinate: CLLocationCoordinate2D, completion: @escaping (String?) -> Void) {
+    let location = CLLocation(latitude: coordinate.latitude, longitude: coordinate.longitude)
+    let geocoder = CLGeocoder()
+
+    geocoder.reverseGeocodeLocation(location) { (placemarks, error) in
+        if let error = error {
+            print("Reverse geocoding error: \(error.localizedDescription)")
+            completion(nil)
+        } else if let placemark = placemarks?.first {
+            // this is to create an accurate point drop
+            var Address: String = ""
+            
+            if let subThoroughfare = placemark.subThoroughfare {
+                Address += subThoroughfare + " "
+            }
+            
+            if let thoroughfare = placemark.thoroughfare {
+                Address += thoroughfare + ", "
+            }
+            
+            if let locality = placemark.locality {
+                Address += locality + ", "
+            }
+            
+            if let state = placemark.administrativeArea {
+                Address += state + ", "
+            }
+            
+            if let postalCode = placemark.postalCode {
+                Address += postalCode
+            }
+            completion(Address)
+        } else {
+            completion(nil)
+        }
+    }
+}
+
+func geocodeLocation(address: String, completion: @escaping (CLLocationCoordinate2D?) -> Void) {
+    let geocoder = CLGeocoder()
+
+    geocoder.geocodeAddressString(address) { (placemarks, error) in
+        if let error = error {
+            print("Geocoding error: \(error.localizedDescription)")
+            completion(nil)
+        } else if let placemark = placemarks?.first, let location = placemark.location {
+            completion(location.coordinate)
+        } else {
+            completion(nil)
+        }
+    }
+}
+
+struct PushtoFirebase_Previews: PreviewProvider {
+    static var previews: some View {
+        PushtoFirebase()
+    }
+}
